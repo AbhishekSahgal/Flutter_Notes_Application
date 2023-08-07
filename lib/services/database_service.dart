a8091210a5e94a70561731fd7505b3baca76a5ce
AbhishekSahgal <ticckusahgal123@gmail.com>
2023-08-07 21:52:22 +0530

    chane in login page commit

---
+++ lib/services/database_service.dart
@@ -0,0 +1,15 @@
+class DatabaseService{
+
+  final String? uid;
+  DatabaseService({this.uid});
+
+  //reference for our collection
+
+  // updating the user data
+  Future updateUserdata(String fullname, string email) async{
+
+  }
+
+
+
+}
